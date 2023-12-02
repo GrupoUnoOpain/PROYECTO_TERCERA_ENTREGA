b3b3486069a2b670eebc8478a9e1da1bd2598f5b
Camilo DC <camilodc7@gmail.com>
2023-12-02 11:50:20 -0500

    Archivo entrega tres proyecto final MINE 4101

--- proyecto_angular/src/app/app.constants.ts
+++ proyecto_angular/src/app/app.constants.ts
@@ -1,6 +1,2 @@
-//export const API_URL = "http://127.0.0.1:8000/auth/token"
-//export const API_URL_MODEL = "http://localhost:8000/"
-
-export const API_URL = "https://522b49aebf2071c3c28838e27dc4a4e9.serveo.net/auth/token"
-export const API_URL_MODEL = "https://522b49aebf2071c3c28838e27dc4a4e9.serveo.net/"
-
+export const API_URL = "http://127.0.0.1:8000/auth/token"
+export const API_URL_MODEL = "http://localhost:8000/"
